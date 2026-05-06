3a1e23bfd32d745b266dda1d7a930b620af9565a
Rohit Seeramsetti <rohitseeramsetti@gmail.com>
2026-05-06 23:29:36 +0530

    fix: update prefix invite URL to granular permissions

--- prefixCommands/invite.js
+++ prefixCommands/invite.js
@@ -1,16 +1,17 @@
 import { EmbedBuilder } from "discord.js";
-const INVITE_URL = "https://discord.com/oauth2/authorize?client_id=1482403011144843366&permissions=8&integration_type=0&scope=bot+applications.commands";
-export const command = {
-    name: "invite",
-    usage: "%invite",
-    description: "Get the invite link to add the bot to your server",
-    async execute(message) {
-        const embed = new EmbedBuilder()
-            .setColor(0x5865f2)
-            .setTitle("➕ Invite Me to Your Server")
-            .setDescription(`[Click here to add me to your server](${INVITE_URL})`)
-            .setFooter({ text: "Thanks for using the bot!" })
-            .setTimestamp();
-        await message.reply({ embeds: [embed] });
-    },
-};
+  const INVITE_URL = "https://discord.com/api/oauth2/authorize?client_id=1482403011144843366&permissions=1394674756822&scope=bot+applications.commands";
+  export const command = {
+      name: "invite",
+      usage: "%invite",
+      description: "Get the invite link to add the bot to your server",
+      async execute(message) {
+          const embed = new EmbedBuilder()
+              .setColor(0x5865f2)
+              .setTitle("➕ Invite Me to Your Server")
+              .setDescription(`[Click here to add me to your server](${INVITE_URL})`)
+              .setFooter({ text: "Thanks for using the bot!" })
+              .setTimestamp();
+          await message.reply({ embeds: [embed] });
+      },
+  };
+  
