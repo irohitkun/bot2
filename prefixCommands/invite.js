af2f72d938e5c05d55b5779c79435d7590078059
Rohit Seeramsetti <rohitseeramsetti@gmail.com>
2026-05-07 01:38:32 +0530

    Update invite.js

--- prefixCommands/invite.js
+++ prefixCommands/invite.js
@@ -1,5 +1,5 @@
 import { EmbedBuilder } from "discord.js";
-  const INVITE_URL = "https://discord.com/api/oauth2/authorize?client_id=1482403011144843366&permissions=1394674756822&scope=bot+applications.commands";
+  const INVITE_URL = "https://discord.com/oauth2/authorize?client_id=1482403011144843366&permissions=8&integration_type=0&scope=bot+applications.commands";
   export const command = {
       name: "invite",
       usage: "%invite",
